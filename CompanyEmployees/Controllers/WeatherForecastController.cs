5d3f33fc912b02661dd7698143394c53be680641
FJRuiz <fjruizperez@marbella.es>
2021-12-13 21:45:03 +0100

    Agregar archivos de proyecto.

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        public WeatherForecastController(IRepositoryManager repository)
+        {
+            _repository =repository
+        }
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            
+        
+            return new string[] { "value 1", "value 2" };
+        }
+        
+    }
+}
